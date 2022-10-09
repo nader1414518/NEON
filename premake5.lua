2f9653c9d9543301f072abaae32a35cc44b1c71c
nader1414518 <49884739+nader1414518@users.noreply.github.com>
2022-10-09 13:42:05 +0200

    Added Premake To Generate Project Files

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Neon"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Neon"
+    location "Neon"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines 
+        {
+            "NEON_PLATFORM_WINDOWS",
+            "NEON_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "NEON_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "NEON_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "NEON_DIST"
+        optimize "On"
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Neon/vendor/spdlog/include",
+        "Neon/src"
+    }
+
+    links
+    {
+        "Neon"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines 
+        {
+            "NEON_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "NEON_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "NEON_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "NEON_DIST"
+        optimize "On"
